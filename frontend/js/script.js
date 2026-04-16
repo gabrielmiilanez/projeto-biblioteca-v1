80996505af348cefdfea4b76fd1f0a517eb7b665
gabrielmiilanez <gabriel.milanez@outlook.com>
2026-04-16 09:09:36 -0300

    :bug: fix: definindo imagem padrão pra quando não for definido nenhum link

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -10,6 +10,10 @@ if (bookContainer) {
                 const div = document.createElement("div");
                 div.classList.add("book");
 
+                if (!book.bookImage || book.bookImage.trim() === "") {
+                    book.bookImage = "https://i.pinimg.com/736x/8a/23/38/8a2338f7e7e137eed4a6c1566fcc476f.jpg";
+                }
+
                 div.innerHTML = `
                     <img class="bookImage" src="${book.bookImage}" alt="${book.title}" />
                     <h3>${book.title}</h3>
